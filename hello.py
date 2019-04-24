b0e7603cec52f864849314c0e29fed90e83e1326
JWEE <j.wagener@amsterdam.nl>
2019-04-24 13:08:48 +0200

    2 commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
